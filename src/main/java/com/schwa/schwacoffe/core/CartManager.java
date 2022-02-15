466f1d674c9ad13d7e158fd5b9a4561a18b63591
Hunter Shaw <huntershaw0@gmail.com>
2022-02-15 13:35:50 -0500

    Set up Initial Code for CartManager

--- src/main/java/com/schwa/schwacoffe/core/CartManager.java
+++ src/main/java/com/schwa/schwacoffe/core/CartManager.java
@@ -2,5 +2,21 @@ package com.schwa.schwacoffe.core;
 
 public class CartManager {
 
+    private volatile static CartManager instance = null;
+
+    private CartManager() {
+
+    }
+
+    public static CartManager getInstance() {
+        if (instance == null) {
+            synchronized (CartManager.class) {
+                if (instance == null) {
+                    instance = new CartManager();
+                }
+            }
+        }
+        return instance;
+    }
 
 }
