16b9db819ccd92b085a2a1e21682217be1566ea7
Hunter Shaw <huntershaw0@gmail.com>
2022-02-14 20:09:06 -0500

    Project structure

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.schwa.schwacoffe {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.schwa.schwacoffe to javafx.fxml;
+    exports com.schwa.schwacoffe;
+}
