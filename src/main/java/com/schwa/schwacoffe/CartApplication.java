4620ddc16f337a685ccf9829984c9a44eceeed53
Hunter Shaw <huntershaw0@gmail.com>
2022-02-15 13:24:41 -0500

    Set Standard Window Size of 800x800

--- src/main/java/com/schwa/schwacoffe/CartApplication.java
+++ src/main/java/com/schwa/schwacoffe/CartApplication.java
@@ -11,7 +11,7 @@ public class CartApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CartApplication.class.getResource("cart-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 800);
         stage.setTitle("Coffee Cart");
         stage.setScene(scene);
         stage.show();
