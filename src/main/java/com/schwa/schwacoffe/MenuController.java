e63e01db564a4a6017f7148311983521de664287
Harlan <harchandler@yahoo.com>
2022-02-16 17:29:29 -0500

    Initial Menu-Branch Creation

---
+++ src/main/java/com/schwa/schwacoffe/MenuController.java
@@ -0,0 +1,15 @@
+package com.schwa.schwacoffe;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class MenuController {
+
+    @FXML
+    private Button CartButton;
+
+    @FXML
+    private TextField CompanyNameField;
+
+}
