750dafc1e60cd3d1ed6f8d6b1b7dd159e3b9a47e
Hunter Kalinoski <HMK30@pitt.edu>
2022-02-16 17:08:17 -0500

    Finished core of the UI and added functionality to radio buttons.

--- src/main/java/com/schwa/schwacoffe/CustomizationController.java
+++ src/main/java/com/schwa/schwacoffe/CustomizationController.java
@@ -1,5 +1,7 @@
 package com.schwa.schwacoffe;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -24,30 +26,17 @@ public class CustomizationController implements Initializable {
                   milkOption1PriceLabel, milkOption2PriceLabel, milkOption3PriceLabel,
                   flavorOption1PriceLabel, flavorOption2PriceLabel, flavorOption3PriceLabel;
     @FXML
-    private ChoiceBox sizeChoiceBox, milkChoiceBox, flavorChoiceBox;
-    @FXML
     private Rectangle TODO_IMAGEVIEW;
     @FXML
-    private ImageView myImageView;
-    @FXML
     private RadioButton sizeRadio1, sizeRadio2, sizeRadio3,
-                        milkRaio1, milkRadio2, milkRadio3;
+                        milkRadio1, milkRadio2, milkRadio3;
     @FXML
     private CheckBox flavorCheckBox1, flavorCheckBox2, flavorCheckBox3;
 
-    ToggleGroup sizeGroup;
+    private ToggleGroup sizeGroup, milkGroup;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-//        myImageView.setImage(new Image((String)));
-
-        //populating choice boxes
-        sizeChoiceBox.getItems().addAll("Small", "Medium", "Large");
-        sizeChoiceBox.setValue("Choose a size:");
-
-        milkChoiceBox.getItems().addAll("Whole", "2%", "Non-fat");
-        milkChoiceBox.setValue("Choose a milk:");
-
 
         //Radio button config
         sizeGroup = new ToggleGroup();
@@ -55,13 +44,54 @@ public class CustomizationController implements Initializable {
         sizeRadio2.setToggleGroup(sizeGroup);
         sizeRadio3.setToggleGroup(sizeGroup);
 
+        milkGroup = new ToggleGroup();
+        milkRadio1.setToggleGroup(milkGroup);
+        milkRadio2.setToggleGroup(milkGroup);
+        milkRadio3.setToggleGroup(milkGroup);
+
+        //listeners for the radio button groups (to update price labels)
+        sizeGroup.selectedToggleProperty().addListener((ob, o, n) -> {
+
+            RadioButton rb = (RadioButton)sizeGroup.getSelectedToggle();
+            SizeGroupChanged(rb);
+        });
+        milkGroup.selectedToggleProperty().addListener((ob, o, n) -> {
+
+            RadioButton rb = (RadioButton)milkGroup.getSelectedToggle();
+            MilkGroupChanged(rb);
+        });
+
+
     }
 
+    public void SizeGroupChanged(RadioButton rb) {
+        RadioGroupChanged(rb, sizePriceLabel);
+    }
+    public void MilkGroupChanged(RadioButton rb) {
+        RadioGroupChanged(rb, milkPriceLabel);
+    }
+
+    public void RadioGroupChanged(RadioButton rb, Label label) {
+        String newText = rb.getText();
+        label.setText(newText);
+    }
 
     @FXML
     public void FinishButtonClicked(ActionEvent event) {
         System.out.println("Finish button was clicked.");
-        System.out.printf("You selected the: " + sizeGroup.getSelectedToggle() + ".\n");       //doesnt print clean option
+        RadioButton selectedSize = (RadioButton)sizeGroup.getSelectedToggle();
+        RadioButton selectedMilk = (RadioButton)milkGroup.getSelectedToggle();
+        if (selectedSize != null)
+            System.out.printf("You selected the: " + selectedSize.getText() + " size.\n");
+        else
+            System.out.println("You did not select a size.");
+        if (selectedMilk != null)
+            System.out.printf("You selected the: " + selectedMilk.getText() + " milk.\n");
+        else
+            System.out.println("You did not select a milk.");
+        System.out.printf("Flavor 1:" + flavorCheckBox1.selectedProperty().getValue() + ".\n");
+        System.out.printf("Flavor 2:" + flavorCheckBox2.selectedProperty().getValue() + ".\n");
+        System.out.printf("Flavor 3:" + flavorCheckBox3.selectedProperty().getValue() + ".\n");
     }
 
     @FXML
