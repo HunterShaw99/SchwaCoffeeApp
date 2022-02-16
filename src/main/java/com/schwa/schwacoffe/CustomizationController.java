e377cbf429766e8462429f2c64ceb79a25129458
Hunter Kalinoski <HMK30@pitt.edu>
2022-02-15 20:05:53 -0500

    Set up project and created customization page skeleton.

---
+++ src/main/java/com/schwa/schwacoffe/CustomizationController.java
@@ -0,0 +1,20 @@
+package com.schwa.schwacoffe;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Rectangle;
+
+public class CustomizationController {
+    @FXML
+    private Button backButton, finishButton;
+    @FXML
+    private Label sizeLabel, milkLabel, flavorLabel,
+                  sizePriceLabel, milkPriceLabel, flavorPriceLabel, totalPriceLabel;
+    @FXML
+    private ChoiceBox sizeChoiceBox, milkChoiceBox, flavorChoiceBox;
+    @FXML
+    private Rectangle TODO_IMAGEVIEW;
+
+}
