dd3eb6d8110124fc00127d1b30c7ee603487936d
Hunter Shaw <huntershaw0@gmail.com>
2022-02-16 13:32:23 -0500

    Pushing Current Work up, Mostly CSS Configuration

--- src/main/java/com/schwa/schwacoffe/CartApplication.java
+++ src/main/java/com/schwa/schwacoffe/CartApplication.java
@@ -12,7 +12,6 @@ public class CartApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CartApplication.class.getResource("cart-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 800);
-        scene.getStylesheets().add("src/main/java/com/schwa/schwacoffe/cart-style.css");
         stage.setTitle("Coffee Cart");
         stage.setScene(scene);
         stage.show();
