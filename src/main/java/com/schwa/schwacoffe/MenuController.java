3163c73b1bbe7a1c47c4764021213c3851fca015
Connor Mowry <cdm70@pitt.edu>
2022-02-18 10:43:54 -0500

    Added initialize method and setup simple events to display hot and cold drinks in controller

--- src/main/java/com/schwa/schwacoffe/MenuController.java
+++ src/main/java/com/schwa/schwacoffe/MenuController.java
@@ -2,12 +2,17 @@ package com.schwa.schwacoffe;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.RadioButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
+import javafx.collections.ObservableList;
+import javafx.collections.FXCollections;
+import javafx.scene.control.cell.ChoiceBoxListCell;
+
+import java.io.IOException;
 
 public class MenuController {
+
+    public static final ObservableList hotDrinks = FXCollections.observableArrayList("Cappuccino", "Pistachio Latte", "Cinnamon Dolce Latte", "White Chocolate Mocha", "Caramel Macchiato");
+    public static final ObservableList coldDrinks = FXCollections.observableArrayList("Salted Caramel Cream Cold Brew", "Iced Coffee", "Iced Espresso", "Iced Pistachio Latte", "Iced Caramel Macchiato");
 
     @FXML
     private ListView MenuListView;
@@ -25,20 +30,25 @@ public class MenuController {
     private ToggleGroup TempButtons;
 
     @FXML
-    private void CartButtonPressed (ActionEvent event) {
+    public void initialize() {
+        HotButton.setSelected(false);
+        ColdButton.setSelected(false);
+    }
+
+    @FXML
+    private void CartButtonPressed (ActionEvent event) throws IOException {
 
     }
 
     @FXML
     private void HotButtonPressed (ActionEvent event) {
-
+        MenuListView.setItems(hotDrinks);
+        MenuListView.setCellFactory(ChoiceBoxListCell.forListView(hotDrinks));
     }
 
     @FXML
     private void ColdButtonPressed (ActionEvent event) {
-
+        MenuListView.setItems(coldDrinks);
+        MenuListView.setCellFactory(ChoiceBoxListCell.forListView(coldDrinks));
     }
-
-
-
 }
