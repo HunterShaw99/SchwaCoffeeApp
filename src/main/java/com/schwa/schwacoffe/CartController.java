b91c325b0f8e6940f59aa06790f0bd5dc8ae3ad5
Hunter Shaw <huntershaw0@gmail.com>
2022-02-18 10:02:25 -0500

    Added Controller to View and Setup Simple Events for Buttons

--- src/main/java/com/schwa/schwacoffe/CartController.java
+++ src/main/java/com/schwa/schwacoffe/CartController.java
@@ -1,5 +1,59 @@
 package com.schwa.schwacoffe;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+
 public class CartController {
 
+    @FXML
+    private Button BackButton;
+
+    @FXML
+    private Label CartLabel;
+
+    @FXML
+    private ListView<?> CartListView;
+
+    @FXML
+    private Label CartTitleLabel;
+
+    @FXML
+    private Label CartTotalLabel;
+
+    @FXML
+    private HBox HeaderBar;
+
+    @FXML
+    private Label PickUpLabel;
+
+    @FXML
+    private Slider PickupSlider;
+
+    @FXML
+    private Button PlaceOrderButton;
+
+    @FXML
+    private Button PlaceOrderButton1;
+
+    @FXML
+    void CancelOrderClicked(MouseEvent event) {
+        System.out.println("Order Canceled");
+    }
+
+    @FXML
+    void MenuButtonClicked(MouseEvent event) {
+        System.out.println("Menu Button Clicked");
+    }
+
+    @FXML
+    void PlaceOrderClicked(MouseEvent event) {
+        System.out.println("Order Placed");
+    }
+
 }
+
