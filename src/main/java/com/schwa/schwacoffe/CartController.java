27509d961f474f55b9f37eded94c5eb3643eec48
Hunter Shaw <huntershaw0@gmail.com>
2022-02-22 18:37:53 -0500

    Removed Irrelevant Code

--- src/main/java/com/schwa/schwacoffe/CartController.java
+++ src/main/java/com/schwa/schwacoffe/CartController.java
@@ -51,27 +51,6 @@ public class CartController {
 
     @FXML
     void CancelOrderClicked(MouseEvent event) {
-
-        CoffeeModel coffee = new CoffeeModel();
-        coffee.setPrice(BigDecimal.valueOf(6.25));
-        coffee.setName("Coffee");
-        coffee.setMilk("Whole");
-        CoffeeModel coffee1 = new CoffeeModel();
-        coffee1.setPrice(BigDecimal.valueOf(2.25));
-        coffee1.setName("Lattee");
-        coffee1.setMilk("non-fat");
-        CoffeeModel coffee3 = new CoffeeModel();
-        coffee3.setPrice(BigDecimal.valueOf(5.25));
-        coffee3.setName("Iced Coffee");
-        coffee3.setMilk("2%");
-        CoffeeModel coffee4 = new CoffeeModel();
-        coffee4.setPrice(BigDecimal.valueOf(4.25));
-        coffee4.setName("Caramel Coffee");
-        coffee4.setMilk("Whole");
-        CartManager.GetInstance().AddBeverage(coffee);
-        CartManager.GetInstance().AddBeverage(coffee1);
-        CartManager.GetInstance().AddBeverage(coffee3);
-        CartManager.GetInstance().AddBeverage(coffee4);
         System.out.println("Order Canceled");
     }
 
