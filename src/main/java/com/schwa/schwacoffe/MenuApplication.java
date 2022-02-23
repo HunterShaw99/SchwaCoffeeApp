2d38d182116b0294ec647f1648c7385b441bdb0f
Hunter Kalinoski <HMK30@pitt.edu>
2022-02-23 13:31:10 -0500

    added menu application

--- src/main/java/com/schwa/schwacoffe/MenuApplication.java
+++ src/main/java/com/schwa/schwacoffe/MenuApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class CartApplication extends Application {
+public class MenuApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(CartApplication.class.getResource("cart-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MenuApplication.class.getResource("Menu.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 800);
-        stage.setTitle("Coffee Cart");
+        stage.setTitle("Schwa Coffee");
         stage.setScene(scene);
         stage.show();
     }
