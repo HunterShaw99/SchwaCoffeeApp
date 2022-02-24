eaa5dcbf4a888ff05da70a6e7d69834dc61a8d43
Hunter Shaw <huntershaw0@gmail.com>
2022-02-24 17:15:52 -0500

    Added some tests for CartManager in testCartManager

--- src/main/test/com/schwa/schwacoffe/core/data/testCartManager.java
+++ src/main/test/com/schwa/schwacoffe/core/data/testCartManager.java
@@ -1,7 +1,40 @@
 package com.schwa.schwacoffe.core.data;
 
+import com.schwa.schwacoffe.models.CoffeeModel;
 import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class testCartManager {
 
+    private CoffeeModel e = new CoffeeModel();
+
+
+    @Test
+    void testEmptyCart() {
+        //test to make sure cart is empty
+        assertEquals(true, CartManager.GetInstance().IsEmpty());
+
+        //test cart isn't empty
+        e.setPrice(BigDecimal.valueOf(5.25));
+        e.setName("Coffee");
+        e.setMilk("Whole");
+        e.setSize("Small");
+        CartManager.GetInstance().AddBeverage(e);
+        assertEquals(false, CartManager.GetInstance().IsEmpty());
+    }
+
+    @Test
+    void testCurrentItem() {
+        //Test no item is selected
+        assertEquals(true, CartManager.GetInstance().GetCurrentItem() == null);
+
+        //Test e is current selected item
+        CartManager.GetInstance().SetCurrentItem(e);
+        assertEquals(true, CartManager.GetInstance().GetCurrentItem().equals(e));
+
+
+    }
 }
