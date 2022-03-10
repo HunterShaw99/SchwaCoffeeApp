cdaeebbba5ed1fb9c9e16c36208aacbdccad7000
Hunter Shaw <huntershaw0@gmail.com>
2022-03-10 17:02:43 -0500

    Created Foundation of OrderModel

--- src/main/java/com/schwa/schwacoffe/models/OrderModel.java
+++ src/main/java/com/schwa/schwacoffe/models/OrderModel.java
@@ -1,13 +1,32 @@
 package com.schwa.schwacoffe.models;
 
+import javafx.collections.ObservableList;
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
 
 public class OrderModel {
 
-    public final BigDecimal orderTotal;
+    private final BigDecimal orderTotal;
+    private final List<CoffeeModel> beverageLIST;
+    private final String orderID;
 
-    public OrderModel(BigDecimal total) {
-        this.orderTotal = total;
+    public OrderModel(BigDecimal orderTotal, ObservableList<CoffeeModel> beverageLIST) {
+        this.orderTotal = orderTotal;
+        this.beverageLIST = beverageLIST;
+        orderID = UUID.randomUUID().toString().substring(0,11);
+    }
+
+    public BigDecimal Get_OrderTotal() {
+        return orderTotal;
+    }
+
+    public ObservableList<CoffeeModel> GetBeverageList() {
+        return (ObservableList<CoffeeModel>) beverageLIST;
+    }
+
+    public String GetOrderID() {
+        return orderID;
     }
 
 }
