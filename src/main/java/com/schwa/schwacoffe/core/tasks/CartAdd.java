6e0d47ae900acea3248a4f8e3500646d5c683137
Hunter Shaw <huntershaw0@gmail.com>
2022-03-17 16:54:52 -0400

    Added NONE for Flavor enum

--- src/main/java/com/schwa/schwacoffe/core/tasks/CartAdd.java
+++ src/main/java/com/schwa/schwacoffe/core/tasks/CartAdd.java
@@ -2,11 +2,13 @@ package com.schwa.schwacoffe.core.tasks;
 
 import com.schwa.schwacoffe.core.data.CartManager;
 import com.schwa.schwacoffe.models.CoffeeModel;
+import com.schwa.schwacoffe.models.constants.Flavor;
 
 public class CartAdd implements Runnable {
     @Override
     public void run() {
         CoffeeModel m = CartManager.GetInstance().GetCurrentItem();
+        if (m.getFlavors().size() == 0) m.addFlavor(Flavor.NONE);
         CartManager.GetInstance().AddBeverage(m);
         m = null;
         CartManager.GetInstance().SetCurrentItem(null);
