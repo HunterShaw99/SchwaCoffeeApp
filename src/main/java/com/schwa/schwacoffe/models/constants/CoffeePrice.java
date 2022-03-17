4243d07ba487b00d081318f16dfd6f15ec9dbfa1
Hunter Shaw <huntershaw0@gmail.com>
2022-03-16 22:48:37 -0400

    Adjusted prices to more accurately represent prices in todays economy

--- src/main/java/com/schwa/schwacoffe/models/constants/CoffeePrice.java
+++ src/main/java/com/schwa/schwacoffe/models/constants/CoffeePrice.java
@@ -5,8 +5,8 @@ import java.math.BigDecimal;
 public abstract class CoffeePrice {
     public static final BigDecimal SMALL_COST = BigDecimal.valueOf(3.45);
     public static final BigDecimal MEDIUM_COST = BigDecimal.valueOf(4.55);
-    public static final BigDecimal LARGE_COST = BigDecimal.valueOf(5.75);
-    public static final BigDecimal FLAVOR_COST = BigDecimal.valueOf(0.25);
-    public static final BigDecimal DAIRY_COST = BigDecimal.valueOf(0.75);
-    public static final BigDecimal NON_DAIRY_COST = BigDecimal.valueOf(1.25);
+    public static final BigDecimal LARGE_COST = BigDecimal.valueOf(5.55);
+    public static final BigDecimal FLAVOR_COST = BigDecimal.valueOf(0.10);
+    public static final BigDecimal DAIRY_COST = BigDecimal.valueOf(0.25);
+    public static final BigDecimal NON_DAIRY_COST = BigDecimal.valueOf(0.75);
 }
